9e13be2dd8f4060db095a26263387dab95134fa4
lakshit-html <lakshitsolanki1234@gmail.com>
2024-10-27 09:52:19 +0530

    commit

--- learn.js
+++ learn.js
@@ -1,7 +1,19 @@
-let a = prompt("Enter a Number: ");
-if (a%5===0){
-    console.log("Number is muntiple of 5")
+let a = prompt("Enter your grade :");
+if (a>=80&& a<=100){
+    console.log("A")
+}
+else if (a>=70&& a<=89){
+    console.log("B")
+}
+else if (a>=60&& a<=69){
+    console.log("C")
+}
+else if (a>=50&& a<=59){
+    console.log("D")
+}
+else if (a>=0&& a<=49){
+    console.log("F")
 }
 else {
-    console.log("else")
+    console.log("Provide grades between 1 to 100")
 }
