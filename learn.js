b2945b9a8b0c79a7f661fbef04f1438d06ba6e23
lakshit-html <lakshitsolanki1234@gmail.com>
2024-10-27 14:09:21 +0530

    fg

--- learn.js
+++ learn.js
@@ -1,19 +1,5 @@
-let a = prompt("Enter your grade :");
-if (a>=90&& a<=100){
-    console.log("A")
-}
-else if (a>=70&& a<=89){
-    console.log("B")
-}
-else if (a>=60&& a<=69){
-    console.log("C")
-}
-else if (a>=50&& a<=59){
-    console.log("D")
-}
-else if (a>=0&& a<=49){
-    console.log("F")
-}
-else {
-    console.log("Provide grades between 1 to 100")
+let lakshit;
+let x = 5;
+for (let a = 1; a <= 10 ; a++){
+    console.log(x,"x",a,"=",x*a)
 }
